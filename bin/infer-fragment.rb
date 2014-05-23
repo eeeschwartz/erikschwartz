51a15eb5ba2dea85dc471d670549dc4e15d5065d
Erik Schwartz <erik@erikschwartz.net>
2014-05-23 15:07:00 -0700

    Fix fn-infer fragment behavior

--- bin/infer-fragment.rb
+++ bin/infer-fragment.rb
@@ -1,11 +1,9 @@
 # search markdown links and convert
-# [do some cool thing](#fn-infer) => [do some cool thing](#do-some-cool-thing)
-
-line = '[do some cool thing](#fn-infer)'
+# [do some cool thing](#fn-infer) => <a name="do-some-cool-thing"></a>[do some cool thing](#do-some-cool-thing)
 def transform(line)
   fn_name = 'fn-infer'
-  md_regex = %r{[^`]\[(.*)\]\(##{fn_name}\)}
-  name_regex = %r{#{fn_name}}
+  md_regex = %r{[^`]?\[(.*)\]\(##{fn_name}\)}
+  name_regex = %r{##{fn_name}}
   a_regex = %r{\[}
 
   p line
@@ -16,22 +14,24 @@ def transform(line)
     # remove non permalink chars
     fragment = fragment.downcase.gsub(/[^a-z-]/, '')
 
-    line = line.sub(name_regex, fragment)
+    line = line.sub(name_regex, "{{page.url}}##{fragment}")
 
-    line = line.sub(a_regex, "<a name=\"#{fragment}\"></a> [")
+    line = line.sub(a_regex, %(<a name="#{fragment}"></a> [))
     p line
   end
+  line
 end
 
-transform(line)
-#
-# File.open(filename) do |f|
-#   out = []
-#
-#   f.each_line do |line|
-#     out << line
-#   end
-#   File.open(filename, 'w') do |f|
-#     out.each { |line| f << line }
-#   end
-# end
+# transform('[do some cool thing](#fn-infer)')
+# exit
+filename = './_posts/2014-05-23-environment-improvements-3.md'
+File.open(filename) do |f|
+  out = []
+
+  f.each_line do |line|
+    out << transform(line)
+  end
+  File.open(filename, 'w') do |f|
+    out.each { |line| f << line }
+  end
+end
